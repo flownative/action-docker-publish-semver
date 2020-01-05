6c4b32b261872cc844c35efb4a6ef236ccc00e1c
Robert Lemke <robert@flownative.com>
2020-01-05 14:09:48 +0100

    Include docker build command

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,23 @@ DOCKER_IMAGE_GITHUB="docker.pkg.github.com/${INPUT_GITHUB_PACKAGE_NAME}:${TAG}"
 echo "${GITHUB_TOKEN}" | docker login -u flownative --password-stdin docker.pkg.github.com
 echo "${INPUT_TARGET_REGISTRY_PASSWORD}" | docker login -u "${INPUT_TARGET_REGISTRY_USERNAME}" --password-stdin "${INPUT_TARGET_REGISTRY_ENDPOINT}"
 
+git checkout ${TAG}
+set -- "-" "${DOCKER_IMAGE_GITHUB}"
+
+BUILD_ENV_SCRIPT=${HOME}/.github/build-env.sh
+
+if [ -f "${BUILD_ENV_SCRIPT}" ]; then
+  # shellcheck disable=SC1090
+  . "${BUILD_ENV_SCRIPT}"
+  IFS="$(printf '\n ')" && IFS="${IFS% }"
+  set -o noglob
+  for line in $(env | grep BUILD_ARG_); do
+    set -- "$@" '--build-arg' "$(echo $line | sed -E 's/(BUILD_ARG_)//g')"
+  done
+fi
+
+docker build "$@" .
+
 docker tag "${DOCKER_IMAGE_GITHUB}" "${INPUT_TARGET_IMAGE_NAME}:${DOCKER_IMAGE_TAG_MAJOR}"
 docker push "${INPUT_TARGET_IMAGE_NAME}:${DOCKER_IMAGE_TAG_MAJOR}"
 docker tag "${DOCKER_IMAGE_GITHUB}" "${INPUT_TARGET_IMAGE_NAME}/:${DOCKER_IMAGE_TAG_MAJOR}.${DOCKER_IMAGE_TAG_MINOR}"
